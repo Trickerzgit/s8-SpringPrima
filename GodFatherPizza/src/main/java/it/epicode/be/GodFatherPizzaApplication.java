731036e1242795e831c219c859b48f21561e219c
Patrick <Patrick@DESKTOP-MR4IAM0.station>
2021-05-17 15:30:12 +0200

    Esercizio GodFather

---
+++ GodFatherPizza/src/main/java/it/epicode/be/GodFatherPizzaApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodFatherPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodFatherPizzaApplication.class, args);
+	}
+
+}
